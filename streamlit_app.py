02c815848f8266e6648a89bb3e2710319a706722
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-06-30 00:07:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 # import boto3, import pandas as pd, from io import StringIO
-
+"""
 def read_csv_from_s3(bucket_name, file_key):
     s3 = boto3.client('s3')
     csv_obj = s3.get_object(Bucket=bucket_name, Key=file_key)
@@ -55,3 +55,4 @@ def main1():
 
 if __name__ == "__main__":
     main1()
+"""
