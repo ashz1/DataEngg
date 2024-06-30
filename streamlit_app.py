c5424bb8d5a545955c0d6136575b5b1bd2947e6d
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-06-30 01:38:28 -0400

    svdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import altair as alt
 
-st.header("Code to import data from S3 server through an EC2 instance")
+
+st.set_page_config(page_title="Data Engineering Project by Aashay", page_icon="❄️", layout="wide")
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+# Enable Altair dark theme for charts
+alt.themes.enable("dark")
+    
+# Custom CSS
+with open('homestyle.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+  
+
+st.write("Code to import data from S3 server through an EC2 instance")
 st.code("""import boto3
 import pandas as pd
 from io import StringIO""")
